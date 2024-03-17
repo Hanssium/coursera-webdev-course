c740362f41fda2394dabf042f4fe0ca6150ecac3
Hanssium <141432674+Hanssium@users.noreply.github.com>
2024-03-17 10:41:10 +0700

    Module 4 Solution

---
+++ module-4/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+	var byeSpeaker = {}, speakWord = "Good Bye";
+
+	byeSpeaker.speak = function(name) {
+		console.log(speakWord + " " + name);
+	}
+	window.byeSpeaker = byeSpeaker;
+})(window);
